db27167db091bd962d0076d66dd2ac2219171354
SumonSN <56505303+SumonSN@users.noreply.github.com>
2022-01-06 02:33:52 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
+import os
+
 OWNER = ""
-BOT_TOKEN = ""
-API_ID = ""
-API_HASH = ""
-SESSION = ""
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+API_ID = int(os.environ.get("API_ID"))
+API_HASH = os.environ.get("API_HASH")
+SESSION = os.environ.get("SESSION")
 
